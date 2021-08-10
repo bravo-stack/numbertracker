05898eac6e44e156409f6122aaff4e4cb7d4e04a
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-08-10 03:10:57 +0100

    it displays country/region

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import json
+from typing import List
 import phonenumbers 
 from phonenumbers import geocoder
 from urllib import response
-from
+from urllib.request import urlopen
+
+phonenumbers00 = phonenumbers.parse("+2349048188177")
+output_info = geocoder.description_for_number(phonenumbers00, 'en')
+
+print(output_info)
+
+url = 'https://ipinfo.io/'
+
+response = urlopen(url)
+
+
+
+
+
+
+
+
+
+
+
